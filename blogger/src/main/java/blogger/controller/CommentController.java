1d8e3dd165dc8ac1aec4a4b987af4a9a43b5f3cc
Mayank Saini <mayank.saini@adda247.com>
2021-07-28 12:19:04 +0530

    comment changes

--- blogger/src/main/java/blogger/controller/CommentController.java
+++ blogger/src/main/java/blogger/controller/CommentController.java
@@ -2,8 +2,13 @@ package blogger.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +17,7 @@ import blogger.dto.CommentResponse;
 import blogger.service.CommentService;
 
 @RestController
-@RequestMapping("users/post/comment")
+@RequestMapping("users/post/{id}/comment")
 public class CommentController {
 	
 	@Autowired
@@ -23,6 +28,11 @@ public class CommentController {
 	{
 		return commentService.getCommentsbyPost(id);
 		
+	}
+	@PostMapping()
+	public ResponseEntity<String> postComment(HttpServletRequest req, @RequestParam (name="comment") String desc,@PathVariable (name="id") Integer id)
+	{
+		return commentService.addComment(req, desc,id);
 	}
 	
 }
--- blogger/src/main/java/blogger/service/CommentService.java
+++ blogger/src/main/java/blogger/service/CommentService.java
@@ -3,13 +3,19 @@ package blogger.service;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import blogger.dto.CommentResponse;
 import blogger.model.Comment;
 import blogger.repository.CommentRepository;
+import blogger.security.JwtTokenProvider;
 
 @Service
 public class CommentService {
@@ -18,6 +24,8 @@ public class CommentService {
 	CommentRepository commentRepository;
 	@Autowired
 	ModelMapper modelMapper;
+	@Autowired
+	 private JwtTokenProvider jwtTokenProvider;
 	
 	public List<CommentResponse> getCommentsbyPost(String id)
 	{
@@ -30,6 +38,16 @@ public class CommentService {
 				  .collect(Collectors.toList());
 		return dtos;
 	}
-		
+		public ResponseEntity<String> addComment(HttpServletRequest req, String desc , Integer id)
+		{	
+			Comment C = new Comment();
+			C.setValue(desc);
+			C.setUsername(jwtTokenProvider.getUsername(jwtTokenProvider.resolveToken(req)));
+			if(C.getUsername()==null)
+				return new ResponseEntity<>("Please login First", HttpStatus.UNAUTHORIZED);
+			C.setPostId(id);
+			commentRepository.save(C);
+			return new ResponseEntity<>("Comment successfully added", HttpStatus.OK);
+		}
 	
 }
